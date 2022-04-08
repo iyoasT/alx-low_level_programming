9acca9998521d8ac1b23d8fb363d5e270fc818b9
iyoasT <itdbgfad@gmail.com>
2022-04-08 09:02:31 -0700

    task 1 commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,12 +9,9 @@ int main(void)
 {
 	int n;
 	int last;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	last = n % 10;
-	
 	if (last > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, last);
