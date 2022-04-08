89695723d250f988b95940cb5cf3ad77381cde14
iyoasT <itdbgfad@gmail.com>
2022-04-08 10:43:04 -0700

    task 9 commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - void
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 48;
+
+	do {
+		putchar(num);
+		if (num!= 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	} while (num <= 57);
+	putchar('\n');
+	return (0);
+}
+
+			
