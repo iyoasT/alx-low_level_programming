9b369c197e4fa370edde6063d0193403b54fa6e6
iyoasT <itdbgfad@gmail.com>
2022-04-08 10:22:54 -0700

    task 3 commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Print the alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char abc = 'a';
+	char ABC = 'A';
+
+	while (abc <= 'z')
+	{
+		putchar(abc);
+		abc++;
+	}
+	whilw (ABC <= 'Z')
+	{
+		putchar(ABC);
+		ABC++;
+	}
+	putchar('\n');
+	return (0);
+}
