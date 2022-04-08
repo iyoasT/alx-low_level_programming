6060181691cc232f1082c5ffae55e8a7c39d4e8a
iyoasT <itdbgfad@gmail.com>
2022-04-08 10:05:25 -0700

    task 6 commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
- * main - Print the alphabet - 'e' and 'q'
+ * main - void
  * Return: 0
  */
 int main(void)
 {
-	int num = 0;
+	int num = 48;
 
-	while (num < 10)
+	while (num <= 57)
 	{
-		printf("%d", num);
+		putchar(num);
 		num++;
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
