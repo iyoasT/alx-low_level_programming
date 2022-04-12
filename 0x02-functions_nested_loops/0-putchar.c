c34dfe4de7822f3e8e6bf0566f4dc254652638f5
iyoasT <itdbgfad@gmail.com>
2022-04-12 05:44:04 -0700

    task 0 commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+include "holberton.h"
+/**
+ * main - prints the string "Holberton" from a character array.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	char c[] = "Holberon";
+	int i;
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
