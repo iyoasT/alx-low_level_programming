b43243a43b22ae7c8afb9d321c605caa4cb9c295
iyoasT <101416874+iyoasT@users.noreply.github.com>
2022-05-06 21:05:24 +0300

    fifth updated commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -6,24 +6,13 @@
  * @argv: args
  * Return: int
  */
-int main(int argc, char *argv[])
+int main(void)
 {
-unsigned long mul;
-int i, j;
-	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
-		}
-
-	}
-	mul = atol(argv[1]) *atol(argv[2]);
-	printf("%lu\n", mul);
-return (0);
+	int a ,b;
+	mul=a*b;
+	printf("enter a number");
+	scanf(&a);
+	printf ("enter a second number");
+	scanf(&b);
+	printf ("the product is:%d" , mul);
 }
